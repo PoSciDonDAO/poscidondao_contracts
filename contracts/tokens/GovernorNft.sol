0e0de858cb7b5384f07ee31299193ca90522eaf2
Marrmee <mhuberts2008@hotmail.com>
2023-06-21 13:43:18 +0200

    Forge for testing and Hardhat to deploy and verify on zkSync

---
+++ contracts/tokens/GovernorNft.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../../lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+
+contract GovernorNft is ERC721 {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public tokenId;
+
+    constructor() ERC721("GovernorNFT", "GovNFT") {
+        
+    }
+
+    function mint(address receiver) external {
+        tokenId.increment();
+        uint256 _tokenId = tokenId.current();
+        _safeMint(receiver, _tokenId);
+    }
+}
---
+++ deploy/deployTrading.ts
@@ -0,0 +1,56 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// load env file
+import dotenv from "dotenv";
+dotenv.config();
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
+
+if (!PRIVATE_KEY)
+  throw "⛔️ Private key not detected! Add it to the .env file!";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Trading contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(PRIVATE_KEY);
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("Trading");
+
+  // Estimate contract deployment fee
+  const donationWallet = "0x690bf2db31d39ee0a88fcac89117b66a588e865a";
+  const treasuryWallet = "0x2Cd5221188390bc6e3a3BAcF7EbB7BCC0FdFC3Fe";
+  const deploymentFee = await deployer.estimateDeployFee(artifact, [donationWallet, treasuryWallet]);
+
+  // ⚠️ OPTIONAL: You can skip this block if your account already has funds in L2
+  // const depositHandle = await deployer.zkWallet.deposit({
+  //   to: deployer.zkWallet.address,
+  //   token: utils.ETH_ADDRESS,
+  //   amount: deploymentFee.mul(2),
+  // });
+  // // Wait until the deposit is processed on zkSync
+  // await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const tradingContract = await deployer.deploy(artifact, [donationWallet, treasuryWallet]);
+
+  //obtain the Constructor Arguments
+  console.log(
+    "constructor args:" + tradingContract.interface.encodeDeploy([donationWallet, treasuryWallet])
+  );
+
+  // Show the contract info.
+  const contractAddress = tradingContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
